96405f717f9bc07c97f8a854fcc93f9ee1b15d21
zacharygolla <zachary.golla@gmail.com>
2020-07-07 15:51:42 -0500

    Buy and Sell Stock 2 solution

--- src/com/leetcode/ArrayEasy.java
+++ src/com/leetcode/ArrayEasy.java
@@ -12,4 +12,13 @@ public class ArrayEasy {
         }
         return i + 1;
     }
+
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
 }
--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    ArrayEasy arrayEasy = new ArrayEasy();
 	    System.out.println(arrayEasy.removeDuplicates(new int[] {1, 1, 2}));
-
+        System.out.println(arrayEasy.maxProfit(new int [] {7,1,5,3,6,4}));
 
     }
 }
