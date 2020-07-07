342372564f854ff5680a0e3fd016552b60a80324
zacharygolla <zachary.golla@gmail.com>
2020-07-07 15:01:20 -0500

    Initial commit

---
+++ src/com/leetcode/Main.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ArrayEasy arrayEasy = new ArrayEasy();
+	    System.out.println(arrayEasy.removeDuplicates(new int[] {1, 1, 2}));
+
+
+    }
+}
