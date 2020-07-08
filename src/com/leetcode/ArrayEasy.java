ebd24c5533ed20b446c295815b48e3087e9fd7a4
zacharygolla <zachary.golla@gmail.com>
2020-07-08 12:45:10 -0500

    rotate array k steps solution

--- src/com/leetcode/ArrayEasy.java
+++ src/com/leetcode/ArrayEasy.java
@@ -37,4 +37,22 @@ public class ArrayEasy {
 
         throw new IllegalArgumentException("No two sum solution");
     }
+
+    public int [] rotate(int [] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for(int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+        return nums;
+    }
 }
--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -9,5 +9,6 @@ public class Main {
 	    System.out.println(arrayEasy.removeDuplicates(new int[] {1, 1, 2}));
         System.out.println(arrayEasy.maxProfit(new int [] {7,1,5,3,6,4}));
         System.out.println(Arrays.toString(arrayEasy.twoSum(new int [] {2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(arrayEasy.rotate(new int [] {1,2,3,4,5,6}, 2)));
     }
 }
